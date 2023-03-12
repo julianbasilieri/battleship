2643e75df1f60e3bc231d43c5277d7894827d1c6
Julian Basilieri <64615928+julianbasilieri@users.noreply.github.com>
2023-03-12 19:17:33 -0300

    funciona

---
+++ battleship.js
@@ -0,0 +1,201 @@
+var maximo_de_barcos = 0
+
+document.getElementById("p1").addEventListener("click", function () {
+    document.getElementById("p1").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p2").addEventListener("click", function () {
+    document.getElementById("p2").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p3").addEventListener("click", function () {
+    document.getElementById("p3").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p4").addEventListener("click", function () {
+    document.getElementById("p4").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p5").addEventListener("click", function () {
+    document.getElementById("p5").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p6").addEventListener("click", function () {
+    document.getElementById("p6").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p7").addEventListener("click", function () {
+    document.getElementById("p7").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p8").addEventListener("click", function () {
+    document.getElementById("p8").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p9").addEventListener("click", function () {
+    document.getElementById("p9").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p10").addEventListener("click", function () {
+    document.getElementById("p10").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p11").addEventListener("click", function () {
+    document.getElementById("p11").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p12").addEventListener("click", function () {
+    document.getElementById("p12").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p13").addEventListener("click", function () {
+    document.getElementById("p13").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p14").addEventListener("click", function () {
+    document.getElementById("p14").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p15").addEventListener("click", function () {
+    document.getElementById("p15").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p16").addEventListener("click", function () {
+    document.getElementById("p16").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p17").addEventListener("click", function () {
+    document.getElementById("p17").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p18").addEventListener("click", function () {
+    document.getElementById("p18").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p19").addEventListener("click", function () {
+    document.getElementById("p19").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p20").addEventListener("click", function () {
+    document.getElementById("p20").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p21").addEventListener("click", function () {
+    document.getElementById("p21").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p22").addEventListener("click", function () {
+    document.getElementById("p22").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p23").addEventListener("click", function () {
+    document.getElementById("p23").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p24").addEventListener("click", function () {
+    document.getElementById("p24").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p25").addEventListener("click", function () {
+    document.getElementById("p25").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p26").addEventListener("click", function () {
+    document.getElementById("p26").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p27").addEventListener("click", function () {
+    document.getElementById("p27").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p28").addEventListener("click", function () {
+    document.getElementById("p28").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p29").addEventListener("click", function () {
+    document.getElementById("p29").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p30").addEventListener("click", function () {
+    document.getElementById("p30").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p31").addEventListener("click", function () {
+    document.getElementById("p31").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p32").addEventListener("click", function () {
+    document.getElementById("p32").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p33").addEventListener("click", function () {
+    document.getElementById("p33").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p34").addEventListener("click", function () {
+    document.getElementById("p34").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p35").addEventListener("click", function () {
+    document.getElementById("p35").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p36").addEventListener("click", function () {
+    document.getElementById("p36").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p37").addEventListener("click", function () {
+    document.getElementById("p37").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p38").addEventListener("click", function () {
+    document.getElementById("p38").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p39").addEventListener("click", function () {
+    document.getElementById("p39").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p40").addEventListener("click", function () {
+    document.getElementById("p40").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p41").addEventListener("click", function () {
+    document.getElementById("p41").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p42").addEventListener("click", function () {
+    document.getElementById("p42").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p43").addEventListener("click", function () {
+    document.getElementById("p43").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p44").addEventListener("click", function () {
+    document.getElementById("p44").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p45").addEventListener("click", function () {
+    document.getElementById("p45").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p46").addEventListener("click", function () {
+    document.getElementById("p46").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p47").addEventListener("click", function () {
+    document.getElementById("p47").className = "white"
+    maximo_de_barcos++
+});
+document.getElementById("p48").addEventListener("click", function () {
+    document.getElementById("p48").className = "white"
+    maximo_de_barcos++
+});
+
+function probar() {
+    if (maximo_de_barcos <= 5) {
+        document.getElementsByClassName("botones").className = "disabled"
+        console.log(maximo_de_barcos)
+    }
+}
